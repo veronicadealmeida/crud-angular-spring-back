892bd1b46fc19f6bb63b6c680376fd7e1b34c210
Veronica <almeida.vrc@gmail.com>
2023-09-14 20:28:57 -0300

    Cria metodo put

--- src/main/java/edu/vda/crud/spring/controller/CourseController.java
+++ src/main/java/edu/vda/crud/spring/controller/CourseController.java
@@ -27,7 +27,7 @@ public class CourseController {
     @GetMapping("/{id}")
     public ResponseEntity<Course> findById(@PathVariable Long id){
         return courseRepository.findById(id)
-                .map(record ->  ResponseEntity.ok().body(record))
+                .map(recordFound ->  ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
@@ -36,6 +36,17 @@ public class CourseController {
     public Course create(@RequestBody Course course){
 
         return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course){
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
     }
 
     @Bean
