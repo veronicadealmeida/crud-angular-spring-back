a563009f7b53657f98f32807f3d384adb41cdd6a
Veronica <almeida.vrc@gmail.com>
2023-09-14 12:21:07 -0300

    Adiciona metodo POST

--- src/main/java/edu/vda/crud/spring/controller/CourseController.java
+++ src/main/java/edu/vda/crud/spring/controller/CourseController.java
@@ -5,10 +5,9 @@ import edu.vda.crud.spring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +22,13 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+
+        return courseRepository.save(course);
     }
 
     @Bean
