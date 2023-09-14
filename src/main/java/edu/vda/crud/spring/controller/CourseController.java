fef92bde0f1306ac789fd3a0c5c8cacfdcfcfba8
Veronica <almeida.vrc@gmail.com>
2023-09-14 20:44:41 -0300

    Cria método delete

--- src/main/java/edu/vda/crud/spring/controller/CourseController.java
+++ src/main/java/edu/vda/crud/spring/controller/CourseController.java
@@ -49,6 +49,15 @@ public class CourseController {
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                }).orElse(ResponseEntity.notFound().<Void>build());
+    }
+
     @Bean
     CommandLineRunner iniDatabase(CourseRepository courseRepository){
         return args -> {
