5ac798c1cb55342e19e18bf5168e42458e41c8c6
Veronica <almeida.vrc@gmail.com>
2023-09-16 17:00:46 -0300

    Adiciona soft delete

--- src/main/java/edu/vda/crud/spring/model/Course.java
+++ src/main/java/edu/vda/crud/spring/model/Course.java
@@ -6,11 +6,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 
 @Data
 @Entity(name = "courses")
+@SQLDelete(sql = "UPDATE courses SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,4 +33,11 @@ public class Course {
     @Pattern(regexp = "back-end|front-end")
     @Column(name = "category", length = 10, nullable = false)
     private String category;
+
+    @NotBlank
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(name = "status", length = 10, nullable = false)
+    private String status = "Ativo";
 }
